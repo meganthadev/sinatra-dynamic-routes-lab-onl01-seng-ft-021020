eb6e774acbdba61e63074064ea799ba21f2abb4b
Megan Horsfield <meganthadev@gmail.com>
2020-04-07 05:42:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,20 +4,20 @@ class App < Sinatra::Base
   
   get '/reversename/:name' do
     @name = params[:name]
-    @reversename =@name.reverse
-    "#{reversename}"
-  end   
-  
+    @reversename = @name.reverse
+    "#{@reversename}"
+  end
+
   get '/square/:number' do
-    @number = params[:number]
-    @square = @number.to_i*@number.to_i 
-    "#{square}"
-  end   
-  
-  get '/say/:number/:phrase'
-    @number = params[:number]
-    @phrase = params[:phrase]
-    "#{@phrase * @number}"
-  end 
+     @number = params[:number]
+     @square = @number.to_i*@number.to_i
+     "#{@square}"
+   end
+
+   get '/say/:number/:phrase' do
+     @number = params[:number].to_i
+     @phrase = params[:phrase]
+     "#{@phrase * @number}"
+   end
 
 end

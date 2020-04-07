890d099190d00a0805cef727c9ef667efe7b49a4
Megan Horsfield <meganthadev@gmail.com>
2020-04-07 05:50:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,5 +34,16 @@ class App < Sinatra::Base
      @number1 = params[:number1].to_i 
      @number2 = params[:number2].to_i
 
-     
+    if @operation == 'add'
+       "#{@number1 + @number2}"
+     elsif @operation == 'subtract'
+       "#{@number1 - @number2}"
+     elsif @operation == 'multiply'
+       "#{@number1 * @number2}"
+     elsif @operation == 'divide'
+       "#{@number1/@number2}"
+     else
+       "Not Valid"
+     end
+   end 
 end

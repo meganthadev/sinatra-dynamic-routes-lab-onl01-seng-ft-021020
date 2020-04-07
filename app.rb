6c5e30d3d6b6185abda9965262a24d9d9205052b
Megan Horsfield <meganthadev@gmail.com>
2020-04-07 05:44:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,5 +19,8 @@ class App < Sinatra::Base
      @phrase = params[:phrase]
      "#{@phrase * @number}"
    end
+   
+   get '/say/:word1/:word2/:word3/:word4/:word5' do 
+     @word1 = params
 
 end
